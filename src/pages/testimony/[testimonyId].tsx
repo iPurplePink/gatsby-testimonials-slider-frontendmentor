54885654426520c0712465164cf1fbe431fa981b
Maridin Lucas San Miguel <maridinsanmiguel@gmail.com>
2021-09-01 21:40:46 +0800

    Add optional chaining

--- src/pages/testimony/[testimonyId].tsx
+++ src/pages/testimony/[testimonyId].tsx
@@ -12,7 +12,7 @@ const Home = ({ testimonyId, location }) => {
   const data = testimonies[currentIndex];
 
   useEffect(() => {
-    if (data.id != testimonyId) {
+    if (data?.id != testimonyId) {
       navigate(`/${testimonies[0].id}`);
     }
   }, []);
@@ -32,14 +32,14 @@ const Home = ({ testimonyId, location }) => {
   return (
     <Layout>
       <SEO
-        title={`${data.firstName} ${data.lastName}`}
-        description={data.testimony}
+        title={`${data?.firstName} ${data?.lastName}`}
+        description={data?.testimony}
         article={true}
       />
       <div className="home-container">
         <div className="photo-container">
           <div className="photo-background">
-            <img className="photo" src={data.photoUrl} alt={data.firstName} />
+            <img className="photo" src={data?.photoUrl} alt={data?.firstName} />
             <div className="action-buttons-container">
               <SpriteIcon
                 onClick={handlePrevClick}
@@ -59,12 +59,12 @@ const Home = ({ testimonyId, location }) => {
             <div className="quotes-icon-container">
               <SpriteIcon className="quotes-icon" symbolId="pattern-quotes" />
             </div>
-            <p className="testimony">&#8220;{data.testimony}&#8221;</p>
+            <p className="testimony">&#8220;{data?.testimony}&#8221;</p>
             <div className="person-details-container">
               <span className="full-name">
-                {data.firstName} {data.lastName}
+                {data?.firstName} {data?.lastName}
               </span>
-              <span className="occupation">{data.occupation}</span>
+              <span className="occupation">{data?.occupation}</span>
             </div>
           </div>
         </div>
