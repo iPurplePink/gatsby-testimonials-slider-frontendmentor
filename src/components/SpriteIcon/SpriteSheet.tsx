675e878cf644389ddbbea98a1b67aae25368f0ac
Maridin Lucas San Miguel <maridinsanmiguel@gmail.com>
2021-09-01 16:53:08 +0800

    Fix props name

--- src/components/SpriteIcon/SpriteSheet.tsx
+++ src/components/SpriteIcon/SpriteSheet.tsx
@@ -10,7 +10,7 @@ const SpriteSheet: FC = () => {
       >
         <path
           fill="#8585AC"
-          fill-rule="evenodd"
+          fillRule="evenodd"
           d="M52.615 0L24.858 100H0V0h52.615zM120 0L92.243 100H67.385V0H120z"
           opacity=".15"
         />
@@ -22,7 +22,7 @@ const SpriteSheet: FC = () => {
       >
         <path
           fill="#F4F4FC"
-          fill-rule="evenodd"
+          fillRule="evenodd"
           d="M610 154C469.493 123.42 358.432 86.087 276.818 42S102.93-11.42 0 14v140h610z"
         />
       </symbol>
@@ -31,7 +31,7 @@ const SpriteSheet: FC = () => {
         viewBox="0 0 697 657"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <g fill="none" fill-rule="evenodd">
+        <g fill="none" fillRule="evenodd">
           <path
             fill="url(#a)"
             d="M233.996 231.082c-62.714 60.37 48.746 169.37 176.07 259.677 127.324 90.307 187.825 151.88 269.763 73.005 81.938-78.874 114.518-247.228 0-332.682-114.518-85.453-383.12-60.369-445.833 0z"
@@ -41,7 +41,7 @@ const SpriteSheet: FC = () => {
             fill="#F4F4FC"
             d="M14.885 304.531c-51.873-49.833 40.32-139.81 145.634-214.355C265.833 15.63 315.877-35.197 383.651 29.912c67.774 65.108 94.722 204.08 0 274.62-94.722 70.539-316.893 49.832-368.766 0z"
           />
-          <g stroke="#D3629D" stroke-width="4">
+          <g stroke="#D3629D" strokeWidth="4">
             <path d="M344 45.087L364.087 25l20.088 20.087L404.262 25l20.088 20.087L444.437 25" />
             <path d="M344 63.087L364.087 43l20.088 20.087L404.262 43l20.088 20.087L444.437 43" />
             <path d="M344 81.087L364.087 61l20.088 20.087L404.262 61l20.088 20.087L444.437 61" />
@@ -58,14 +58,14 @@ const SpriteSheet: FC = () => {
         viewBox="0 0 13 18"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <path fill="none" stroke="#8585AC" stroke-width="3" d="M2 1l8 8-8 8" />
+        <path fill="none" stroke="#8585AC" strokeWidth="3" d="M2 1l8 8-8 8" />
       </symbol>
       <symbol
         id="icon-prev"
         viewBox="0 0 12 18"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <path fill="none" stroke="#8585AC" stroke-width="3" d="M11 1L3 9l8 8" />
+        <path fill="none" stroke="#8585AC" strokeWidth="3" d="M11 1L3 9l8 8" />
       </symbol>
     </svg>
   );
